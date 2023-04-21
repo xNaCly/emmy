8fabe9a0dbf9e40519bbd84abe83f892347a5894
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-04-21 20:43:40 +0200

    feat: add repl and computation mode

---
+++ emmy.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/xNaCly/emmy/consts"
+	"github.com/xNaCly/emmy/lexer"
+)
+
+func debug_printToken(t []consts.Token) {
+	for _, v := range t {
+		fmt.Printf("[%s][%d][%v]\n", consts.KIND_LOOKUP[v.Kind], v.Pos, v.Content)
+	}
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		debug_printToken(lexer.NewScanner().NewInput(os.Args[1]).Start())
+		return
+	}
+	log.Println("welcome to the emmy repl")
+	prompt := "E> "
+	reader := bufio.NewReader(os.Stdin)
+	l := lexer.NewScanner()
+	for {
+		fmt.Print(prompt)
+		line, _ := reader.ReadString('\n')
+		debug_printToken(l.NewInput(strings.TrimSpace(line)).Start())
+	}
+}
