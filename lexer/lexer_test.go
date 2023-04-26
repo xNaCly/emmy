b6e93576e23b2c985fc3e820a085544bfa6e5bfd
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-04-26 17:33:06 +0200

    test(lexer): now testing everything

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,38 +2,97 @@ package lexer
 
 import (
 	"testing"
+
+	"github.com/xNaCly/emmy/consts"
 )
 
-func TestLexerError(t *testing.T) {
-	l := len(NewScanner().NewInput("test").Start())
-	if l != 0 {
-		t.Errorf("got %d, wanted %d", l, 0)
+// iterates over key value pairs in m, calls testing.Errorf if the matcher function returns false for ok
+func testHelper(t *testing.T, m map[string]any, matcher func(k string, v any) (bool, any)) {
+	for k, v := range m {
+		if ok, val := matcher(k, v); !ok {
+			t.Errorf("wanted %v, got %v", v, val)
+		}
 	}
 }
 
+func TestLexerError(t *testing.T) {
+	tests := map[string]any{
+		"test":     0,
+		"!":        0,
+		"=":        0,
+		"x":        0,
+		"aoadojad": 0,
+	}
+
+	s := NewScanner()
+
+	testHelper(t, tests, func(k string, v any) (bool, any) {
+		o := len(s.NewInput(k).Start())
+		return o == v, o
+	})
+}
+
 func TestLexerNumbers(t *testing.T) {
-	tests := map[string]float64{
-		"1":                    1,
+	tests := map[string]any{
+		"1":                    1.0,
 		"0.25":                 0.25,
 		"1.25":                 1.25,
 		"1.2_5":                1.25,
 		"1129_1029.2_5_1209_0": 11291029.251209,
 	}
+
 	s := NewScanner()
-	for k, v := range tests {
+
+	testHelper(t, tests, func(k string, v any) (bool, any) {
 		o := s.NewInput(k).Start()[0].Content.(float64)
-		// INFO: comparing floats only really working by subtracting them
-		// and checking if the reminder is within the defined acceptable margin
-		if (o - v) > 0.01 {
-			t.Errorf("got %f, wanted %f - diff: %f", o, v, o-v)
-		}
-	}
+		return (o - v.(float64)) < 0.01, o
+	})
 }
 
 func TestLexerSingleChar(t *testing.T) {
+	tests := map[string]any{
+		"0": consts.NUMBER,
+		"+": consts.PLUS,
+		"-": consts.MINUS,
+		"/": consts.DIVISION,
+		"*": consts.MULTIPLICATION,
+		"^": consts.POWER,
+		",": consts.COMMA,
+		";": consts.SEMICOLON,
+		"(": consts.PARENOPEN,
+		")": consts.PARENCLOSE,
+		"[": consts.BRACKETOPEN,
+		"]": consts.BRACKETCLOSE,
+		"{": consts.BRACEOPEN,
+		"}": consts.BRACECLOSE,
+	}
 
+	s := NewScanner()
+
+	testHelper(t, tests, func(k string, v any) (bool, any) {
+		o := s.NewInput(k).Start()[0].Kind
+		return o == v, o
+	})
 }
 
 func TestLexerBuildIn(t *testing.T) {
+	tests := map[string]any{
+		"@sin":  consts.SIN,
+		"@sqrt": consts.SQRT,
+		"@e":    consts.E,
+		"@phi":  consts.PHI,
+		"@pi":   consts.PI,
+		"@cos":  consts.COS,
+		"@tan":  consts.TAN,
+		"@lb":   consts.LB,
+		"@ln":   consts.LN,
+		"@lg":   consts.LG,
+	}
 
+	s := NewScanner()
+
+	testHelper(t, tests, func(k string, v any) (bool, any) {
+		o := s.NewInput(k).Start()[0].Kind
+		return o == v, o
+	})
 }
